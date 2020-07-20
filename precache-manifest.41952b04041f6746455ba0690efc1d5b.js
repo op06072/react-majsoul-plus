77ae2a5beffc4f123bf538e3546162ebe563a9f7
op06072 <op06072@gmail.com>
2020-07-20 18:07:52 +0900

    Updates

---
+++ precache-manifest.41952b04041f6746455ba0690efc1d5b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e23f3f0bb44e64ee6ca75c172c73cb0",
+    "url": "/react-majsoul-plus/index.html"
+  },
+  {
+    "revision": "3a6378dc74591c68656b",
+    "url": "/react-majsoul-plus/static/css/main.dc29bab4.chunk.css"
+  },
+  {
+    "revision": "2312c3c25230c9dbb1a6",
+    "url": "/react-majsoul-plus/static/js/2.1356f608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-majsoul-plus/static/js/2.1356f608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6378dc74591c68656b",
+    "url": "/react-majsoul-plus/static/js/main.de02bcd6.chunk.js"
+  },
+  {
+    "revision": "cdc4cb25ba95d4b49e29",
+    "url": "/react-majsoul-plus/static/js/runtime-main.7bd18f63.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/react-majsoul-plus/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/react-majsoul-plus/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/react-majsoul-plus/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/react-majsoul-plus/static/media/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "424662ea34034a88ef8e8e50d8587a24",
+    "url": "/react-majsoul-plus/static/media/SourceHanSansCN-Light.424662ea.otf"
+  },
+  {
+    "revision": "4bf258e4fc46ebec0711833d027dae88",
+    "url": "/react-majsoul-plus/static/media/SourceHanSansCN-Normal.4bf258e4.otf"
+  },
+  {
+    "revision": "1f649d2bea51807a5b730e44b6443781",
+    "url": "/react-majsoul-plus/static/media/iconfont.1f649d2b.woff"
+  },
+  {
+    "revision": "27182c1f0100412d5926568c00fdb5fc",
+    "url": "/react-majsoul-plus/static/media/iconfont.27182c1f.eot"
+  },
+  {
+    "revision": "9a609b68a866e5cc197bb29270bc9af5",
+    "url": "/react-majsoul-plus/static/media/iconfont.9a609b68.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-majsoul-plus/static/media/logo.5d5d9eef.svg"
+  }
+]);
